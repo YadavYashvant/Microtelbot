398da917ced3cee0d068fd3afff710b0bf170aed
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-24 23:45:44 +0530

    Initial commit

---
+++ src/main/java/com/yashvant/microtelbot/MicrotelbotApplication.java
@@ -0,0 +1,13 @@
+package com.yashvant.microtelbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrotelbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrotelbotApplication.class, args);
+    }
+
+}
