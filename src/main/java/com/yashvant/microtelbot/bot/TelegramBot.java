65d09621ba9a23161f087ecc3643185c03be9858
YadavYashvant <yashvantyadav855@gmail.com>
2024-07-26 13:49:06 +0530

    feature: added sticker support

--- src/main/java/com/yashvant/microtelbot/bot/TelegramBot.java
+++ src/main/java/com/yashvant/microtelbot/bot/TelegramBot.java
@@ -3,13 +3,13 @@ package com.yashvant.microtelbot.bot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
+@Slf4j
 public class TelegramBot extends TelegramLongPollingBot {
 
     @Value("${bot.name}")
@@ -20,24 +20,36 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
-        if(update.hasMessage()){
+        if (update.hasMessage()) {
             Message incomingMessage = update.getMessage();
             Long chatId = incomingMessage.getChatId();
 
-            if(incomingMessage.hasText()){
+            if (incomingMessage.hasText()) {
                 String text = incomingMessage.getText();
 
-                if(text.equals("/start")){
-                    sendMessage(chatId, "Hello! I am mobotronst. I am here to help you.");
-                } else {
-                    sendMessage(chatId, "I am sorry, I do not understand that command.");
+                switch (text) {
+                    case "/start":
+                        sendMessage(chatId, "Hello! I am " + botName + ". I am here to help you, type /help to see the list of commands.");
+                        break;
+                    case "/help":
+                        sendMessage(chatId, "Here are the list of commands: \n /start - Start the bot \n /help - List of commands \n /echo - Echo the message \n /time - Get the current time");
+                        break;
+                    case "/echo":
+                        sendMessage(chatId, "You said: " + text);
+                        break;
+                    case "/time":
+                        sendMessage(chatId, "The current time is: " + System.currentTimeMillis());
+                        break;
+                    default:
+                        sendMessage(chatId, "I am sorry, I do not understand that command.");
+                        break;
                 }
-            }else{
+            } else if (incomingMessage.hasSticker()) {
+                sendMessage(chatId, "Nice sticker!");
+            } else {
                 sendMessage(chatId, "Invalid message. Please send a text message.");
             }
         }
-
     }
 
     @Override
@@ -50,17 +62,15 @@ public class TelegramBot extends TelegramLongPollingBot {
         return botToken;
     }
 
-    public void sendMessage(Long chatId, String sentMessage){
+    public void sendMessage(Long chatId, String sentMessage) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(sentMessage);
 
-        try{
+        try {
             execute(message);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Failed to send message", e);
         }
-
     }
-
 }
